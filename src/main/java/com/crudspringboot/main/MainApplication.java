4a5e76a2ef9d0f4d7dc9556c46da310e5a3769b9
soynain <mrhades9000@gmail.com>
2024-07-27 23:07:58 -0600

    MNSG | DAY 1 | Teniendo rato sin ocupar spring. Acordandome de los properties y los on ApplicationEvents. Falta repasar el concepto de beans y autowired. Estoy seguro que a diferencia de tiempos anteriores, podre absorverlo mejor

--- src/main/java/com/crudspringboot/main/MainApplication.java
+++ src/main/java/com/crudspringboot/main/MainApplication.java
@@ -1,13 +1,26 @@
 package com.crudspringboot.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication implements ApplicationListener<ContextRefreshedEvent>{
 
+	 static Logger logger 
+        = LoggerFactory.getLogger(MainApplication.class); 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+
+		
+	}
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		// TODO Auto-generated method stub
+		logger.info("HOLA MUNDO SOY MOISEXY Y ESTA ES MI PRIMERA PRUEBA");
 	}
 
 }
