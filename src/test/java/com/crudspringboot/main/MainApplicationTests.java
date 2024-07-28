1121bdc9bb3090c587be5b064e83252e988642dc
soynain <mrhades9000@gmail.com>
2024-07-27 22:37:59 -0600

    First one

---
+++ src/test/java/com/crudspringboot/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudspringboot.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
